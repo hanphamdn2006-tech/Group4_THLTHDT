de26a2c5d22b40bba9f4a85db6a2d5b60f4c467d
TRANHUYENTRANG376 <ct0905641972@gmail.com>
2025-10-21 22:28:34 +0700

    Them_Trang_Tuan7

---
+++ BTH_Tuan7_BaiCuaTrang/SachTieuThuyet.java
@@ -0,0 +1,35 @@
+public class SachTieuThuyet extends Sach {
+    private String theLoai;       
+    private boolean laSachSeries; 
+    public SachTieuThuyet() {
+        super();
+    }
+    public SachTieuThuyet(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, double giaCoBan, String theLoai, boolean laSachSeries) {
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong, giaCoBan);
+        this.theLoai = theLoai;
+        this.laSachSeries = laSachSeries;
+    }
+    public String getTheLoai() {
+        return theLoai;
+    }
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
+    public boolean isLaSachSeries() {
+        return laSachSeries;
+    }
+    public void setLaSachSeries(boolean laSachSeries) {
+        this.laSachSeries = laSachSeries;
+    }
+    @Override
+    public double tinhGiaBan() {
+        return getGiaCoBan() + (laSachSeries ? 15000:0 );
+    }
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nThe loai: " + theLoai +
+                "\nLa sach series: " + (laSachSeries ? "Co" : "Khong") +
+                "\nGia ban: " +  tinhGiaBan();
+    }
+}
