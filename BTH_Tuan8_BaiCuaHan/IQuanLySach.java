70253e995f9dffd527800ee7b2ba9891f8f00e83
hanphamdn2006-tech <hanpham.dn.2006@gmail.com>
2025-10-28 11:24:44 +0700

    Them_Han_Tuan8

---
+++ BTH_Tuan8_BaiCuaHan/IQuanLySach.java
@@ -0,0 +1,9 @@
+
+
+public interface IQuanLySach {
+    void themSach(Sach s);
+    Sach timKiemSach(String tieuDe); 
+    boolean xoaSach(String maSach);
+    void hienThiDanhSach();
+    Sach timKiemTheoMa(String maSach); 
+}
