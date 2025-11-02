4cb4c5ae5020903802e05f93534586a994017040
hanphamdn2006-tech <hanpham.dn.2006@gmail.com>
2025-11-02 14:03:37 +0700

    Them_Han_Tuan8

---
+++ BTH_Tuan8_BaiCuaHan/Sach.java
@@ -0,0 +1,109 @@
+public abstract class Sach implements IGiaBan, IKiemKe {
+
+    protected String maSach;
+    protected String tieuDe;
+    protected String tacGia;
+    protected int namXuatBan;
+    protected int soLuong;
+    protected double giaCoBan;
+    protected String viTri;
+    
+
+
+    public Sach () {};
+
+    public Sach (String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, double giaCoBan, String viTri) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+        this.giaCoBan = giaCoBan;
+        this.viTri = viTri;
+    }
+
+
+    public String getViTri() {
+        return viTri;
+    }
+    public void setViTri(String viTri) {
+        this.viTri = viTri;
+    }       
+
+    public String getmasach () {
+        return maSach;
+    }
+    public void setmasach (String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String gettieude () {
+        return tieuDe;
+    }
+    public void settieude (String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+
+    public String gettacgia () {
+        return tacGia;
+    }
+    public void settacgia (String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public int getnamxuatban () {
+        return namXuatBan;
+    }
+    public void setnamxuatban (int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+
+    public int getsoluong () {
+        return soLuong;
+    }
+    public void setsoluong (int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+     public double getGiaCoBan() {
+        return giaCoBan;
+    }
+
+    public void setGiaCoBan(double giaCoBan) {
+        this.giaCoBan = giaCoBan;
+    }
+
+
+    public void hienThiThongTin ()
+    {
+        System.out.println (this.toString());
+        System.out.println ("------------------------------------");
+    }   
+
+    public abstract double tinhGiaBan();
+
+    @Override
+    public boolean kiemTraTonKho(int soLuongToiThieu) {
+        return this.soLuong>=soLuongToiThieu;
+    }
+
+    @Override
+    public void capNhatViTri(String viTriMoi) {
+        this.viTri=viTriMoi;
+        System.out.println("Da chuyen sach" + tieuDe + "den khu vuc:" + viTriMoi);
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Mã sách: " + maSach +
+                ", Tên sách: " + tieuDe +
+                ", Tác giả: " + tacGia +
+                ", Năm xuất bản: " + namXuatBan +
+                ", Số lượng: "+ soLuong+
+                ", Vị trí: " + viTri +
+                ", Giá cơ bản: " + giaCoBan + " VNĐ";
+    }
+
+}
+
